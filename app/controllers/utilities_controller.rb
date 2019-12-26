4438f817b469033ce12e322c5f51cf049703668c
Spiros Avlonitis <spirosa84@hotmail.com>
2019-12-26 03:50:56 +0200

    new utility

--- app/controllers/utilities_controller.rb
+++ app/controllers/utilities_controller.rb
@@ -8,4 +8,34 @@ class UtilitiesController < ApplicationController
 
   def show
   end
+
+  def create
+      @utility= Utility.new(
+            company: utility_params[:company],
+            amount: utility_params[:amount],
+            date_issued: utility_params[:date_issued],
+        )
+
+      respond_to do |format|
+        begin
+            @utility.save
+            format.html { redirect_to '/', notice: 'Utility was successfully created.' }
+            format.json { render :show, status: :created, location: root }
+            
+        rescue ActiveRecord::NotNullViolation => e
+            puts e.class
+            format.html { redirect_to '/', notice: 'Utility was not created' }
+            format.json { render :show, status: :unprocessable_entity, location: root }
+        
+        end
+      end
+  end
+
+  private
+
+    def utility_params
+        params.fetch(:utility, :company, :amount, :date_issued,{})
+    end
+
+
 end
