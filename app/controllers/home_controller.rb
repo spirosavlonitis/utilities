3f315abc4f2fa56d4fb15572814ce5beb2959dfb
Spiros Avlonitis <spirosa84@hotmail.com>
2019-12-28 03:12:48 +0200

    by year

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,16 @@ class HomeController < ApplicationController
     def index
 #        p current_user.utilities[0].id, "here"
 
-        @utilities = {}
+        @utilities_2019 = {}
         current_user.utilities.each do |utility|
-            p utility
-            @utilities[utility.date_issued] = [
-                utility.company, utility.amount
-            ]
+            case utility.date_issued.year.to_i
+                when 2019
+                    p utility.date_issued.year
+                    @utilities_2019[utility.date_issued] = [
+                        utility.company, utility.amount
+                    ]
+            end
         end
-        @utilities = @utilities.to_json.html_safe
+        @utilities_2019 = @utilities_2019.to_json.html_safe
     end
 end
