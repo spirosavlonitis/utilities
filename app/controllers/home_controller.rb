88fe282a4ab01e042a5201047fe44b19f13cd166
Spiros Avlonitis <spirosa84@hotmail.com>
2019-12-29 03:14:08 +0200

    improved code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,20 @@ class HomeController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        @utilities_2019 = {}
+        @fields = {2019 => {}, 2020 => {}}
+        @utilities = { 2019 => {}, 2020 => {} }
         current_user.utilities.each_with_index do |utility, i|
-            case utility.date_issued.year.to_i
-                when 2019
-                    @utilities_2019["2019_#{i}"] = [
-                        utility.company, utility.amount, utility.date_issued
-                    ]
-            end
+            @utilities[utility.date_issued.year.to_i][i] = [
+                utility.company, utility.amount, utility.date_issued
+            ]
+            @fields[2019][utility.company] = 0 if @fields[2019][utility.company] == nil
         end
-        @total_2019 = @utilities_2019.values.sum { |e| e[1] }
-        @utilities_2019 = @utilities_2019.to_json.html_safe
+        @total_2019 = @utilities[2019].values.sum do |e| 
+            @fields[2019][e[0]] += e[1]             # add up yearly cost
+            e[1]
+        end
+        @utilities.each do |year, values|
+            @utilities[year] = @utilities[year].to_json.html_safe
+        end
     end
 end
